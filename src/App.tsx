b0164e71e488e6ac21585137ea48e668be5f0a05
Miguel <pilouco2602@gmail.com>
2024-08-10 17:12:26 +0200

    tabela

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Home from "./home/home";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './home/home';
+import ReactVirtualizedTable from "./view/Table";
+import TwoThousandTwentyTwo from "./view/2022";
+
 
 function App() {
     return (
-        <Home></Home>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/table" element={<ReactVirtualizedTable />} />
+                <Route path="/2022" element={<TwoThousandTwentyTwo />} />
+            </Routes>
+        </Router>
     );
 }
 
